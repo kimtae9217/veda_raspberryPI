55ed2fbd1241eaac75fafb24a90534fa7b32595c
Taewon Kim <kimtae9217@gmail.com>
2024-08-28 10:42:55 +0900

    add spawn.c - use posix_spawn() and spawn attributes

---
+++ code/spawn.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <spawn.h>
+
+extern char **environ;
+
+int system(char *cmd)
+{
+	pid_t pid;
+	int status;
+	posix_spawn_file_actions_t actions;
+	posix_spawnattr_t attrs;
+	char *argv[] = {"sh", "-c", cmd, NULL};
+
+	posix_spawn_file_actions_init(&actions);
+	posix_spawnattr_init(&attrs);
+	posix_spawnattr_setflags(&attrs, POSIX_SPAWN_SETSCHEDULER);
+	posix_spawn(&pid, "/bin/sh", &actions, &attrs, argv, environ);
+	waitpid(pid, &status, 0);
+
+	return status;
+}
+
+int main(int argc, char **argv, char **envp)
+{
+	while(*envp)
+		printf("%s\n", *envp++);
+
+	system("who");
+	system("nocommand");
+	system("cal");
+
+	return 0;
+
+}
