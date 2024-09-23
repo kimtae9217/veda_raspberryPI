26ef67475c0d8f35da8efdd43ceb12b8d1efc869
Taewon Kim <kimtae9217@gmail.com>
2024-09-23 16:54:44 +0900

    2024-09-23 wiringPi를 이용한 GPIO 프로그래밍 코드

---
+++ raspberrypi_unix/chapter3/toggleswitch.c
@@ -0,0 +1,34 @@
+#include <wiringPi.h>
+#include <stdio.h>
+
+#define SW 5
+#define LED 1
+
+int switchControl()
+{
+    int i;
+    pinMode(SW, INPUT);
+    pinMode(LED, OUTPUT);
+    int ledstate = 0;
+    int lastButtonState = HIGH;
+    int buttonState;
+
+    for(;;){
+        buttonState = digitalRead(SW);
+        if(digitalRead(SW) == LOW && lastButtonState == HIGH){
+            ledstate = !ledstate;
+            digitalWrite(LED, ledstate);
+            delay(200);
+        }
+        buttonState = ledstate;
+        delay(10);
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    wiringPiSetup();
+    switchControl();
+    return 0;
+}
