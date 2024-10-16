bdb02d216f345f4b132443733b26047508283011
Taewon Kim <kimtae9217@gmail.com>
2024-10-16 17:22:03 +0900

    tool test code

---
+++ raspberrypi_unix/tool/valgrind/memorytest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void memory_leak(){
+    int ndx;
+    char *p_leak;
+    p_leak = malloc(10);
+    //for(ndx = 0; ndx <= 10; ndx++);
+    for(ndx = 0; ndx < 10; ndx++);
+        p_leak[ndx] = 'a';
+    for(ndx = 0; ndx < 10; ndx++)
+        printf("%c", p_leak[ndx]);
+    //free(p_leak);
+}
+
+int main()
+{
+    memory_leak();
+}
